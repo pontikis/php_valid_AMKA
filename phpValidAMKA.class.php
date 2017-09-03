9e0a67d6eb4d1c069a740b4d25732440f9e9f8b7
Christos Pontikis <pontikis@gmail.com>
2017-09-03 19:03:41 +0300

    basic functionality #1

--- phpValidAMKA.class.php
+++ phpValidAMKA.class.php
@@ -4,7 +4,7 @@
  * Class phpValidAMKA
  *
  * a simple php class to validate Greek Social Security Number (AMKA)
- * https://github.com/pontikis/phpS3Simple
+ * https://github.com/pontikis/php_valid_AMKA
  *
  * @author     Christos Pontikis http://pontikis.net
  * @copyright  Christos Pontikis
@@ -13,30 +13,69 @@
  *
  */
 class phpValidAMKA {
+	/**
+	 * @var null|string one of 'male'. 'female'
+	 */
+	private $gender;
+	/**
+	 * @var null|string DDMMYY
+	 */
+	private $date_of_birth;
+	/**
+	 * @var null|integer (a positive integer)
+	 */
+	private $date_of_birth_years_ago;
 
-	private $ds;
-	private $s3_client;
 	private $last_error;
 
 	public function __construct(array $options) {
 
+		// options -------------------------------------------------------------
+		$defaults = array(
+			'gender' => null,
+			'date_of_birth' => null,
+			'date_of_birth_years_ago' => 120
+		);
+
+		$opt = array_merge($defaults, $options);
+
+		// optional
+		$this->gender = $opt['gender'];
+		$this->date_of_birth = $opt['date_of_birth'];
+		$this->date_of_birth_years_ago = $opt['date_of_birth_years_ago'];
+
+		// error ---------------------------------------------------------------
 		$this->last_error = null;
 
 	}
 
+	// public functions - getters ----------------------------------------------
 
 	public function getLastError() {
 		return $this->last_error;
 	}
 
 
-	public function validateAMKA() {
+
+	// public functions - setters ----------------------------------------------
+
+	/**
+	 * Set option
+	 *
+	 * @param $opt
+	 * @param $val
+	 */
+	public function setOption($opt, $val) {
+		$this->$opt = $val;
+	}
+
+	// public functions - main methods -----------------------------------------
+	public function validateAMKA($amka) {
 
 
 		return true;
 
 	}
-
 
 
 
