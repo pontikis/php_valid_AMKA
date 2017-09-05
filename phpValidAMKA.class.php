68a9fb5a9a314266afb957db3bdd6b0c12e2476f
Christos Pontikis <pontikis@gmail.com>
2017-09-05 18:33:52 +0300

    docs

--- phpValidAMKA.class.php
+++ phpValidAMKA.class.php
@@ -9,7 +9,7 @@
  * @author     Christos Pontikis http://pontikis.net
  * @copyright  Christos Pontikis
  * @license    MIT http://opensource.org/licenses/MIT
- * @version    0.7.0 (03 Sep 2017)
+ * @version    0.1.0 (05 Sep 2017)
  *
  */
 class phpValidAMKA {
